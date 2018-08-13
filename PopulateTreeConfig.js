9be2ec28b5550c714c7b09b7350bcbaf8266e377
babak35102 <babak35102@gmail.com>
2018-08-13 15:17:57 +0430

    completeTreeOfConfig

--- PopulateTreeConfig.js
+++ PopulateTreeConfig.js
@@ -175,6 +175,7 @@ function SortFirstListBaseOnSecond(firstWords, preferedItems) {
 
 function AddThisLineToConfigTree(wordsInThisLine){
     var precededWords = [];
+    wordsInThisLine = DivideInterfacesIntoSubString(wordsInThisLine);
     for(var i = 0; i < wordsInThisLine.length; i++){
         AddNode(precededWords, wordsInThisLine[i])
         precededWords.push(wordsInThisLine[i]);
@@ -204,11 +205,6 @@ function FindParentOfANode(precededNodesList, node){   //FindParentOfANode(["ser
 }
 
 
-var linesOfConfig = configfile.split("\n");
-var listOfFirstWords = GetFirstWordsOfConfig(linesOfConfig);
-var preferedList = ["interface", "ip", "controller", "line", "hostname", "username", "enable", "access-list", "router", "clock", "license", "version"];
-var sortedFirstWords = SortFirstListBaseOnSecond(listOfFirstWords, preferedList);
-//var childOfEthernet0 = rootConfig.children[2].getChildren();
 
 function InsertNodeUnderItsParent(node, parent){
     var childsOfParent = parent.children;
@@ -219,15 +215,58 @@ function InsertNodeUnderItsParent(node, parent){
             break;
         }
     }
-    if(isBetweenChildsAlready == true) {
-        console.log(node + " already exist for this parent")
-    }
-    else{
+    if(isBetweenChildsAlready == false) {
         parent.addChild(new Node(node));
     }
+    
+}
+
+function DivideInterfacesIntoSubString(wordsInThisLine) {
+    var thisLineOfCode = [];
+    thisLineOfCode.push(wordsInThisLine[0]);
+
+    for (var i = 1; i < wordsInThisLine.length; i++) {
+        var word = wordsInThisLine[i];
+        var j = 0;
+        var findDigit = false;
+        while (j < word.length) {
+            if (word.charCodeAt(j) >= 48 && word.charCodeAt(j) <= 57) {
+                if (word.charCodeAt(j - 1) >= 65 && word.charCodeAt(j - 1) <= 122) {
+                    findDigit = true;
+                    thisLineOfCode.push(word.substring(0, j));
+                    word = word.substring(j, word.length);
+                    var indexOfdot = word.indexOf(".");
+                    if(indexOfdot == -1) {
+                        thisLineOfCode.push(word);
+                    }
+                    else {
+                        thisLineOfCode.push(word.substring(0, indexOfdot));
+                        thisLineOfCode.push(word.substring(indexOfdot, word.length));
+                    }
+                    
+                    break;
+                }
+            }
+            j++;
+
+        }
+
+        if(findDigit == false){
+            thisLineOfCode.push(word);           
+        }
+    }
+    return thisLineOfCode
 }
 
 
+var linesOfConfig = configfile.split("\n");
+var listOfFirstWords = GetFirstWordsOfConfig(linesOfConfig);
+var preferedList = ["interface", "ip", "controller", "line", "hostname", "username", "enable", "access-list", "router", "clock", "license", "version"];
+var sortedFirstWords = SortFirstListBaseOnSecond(listOfFirstWords, preferedList);
+console.log("salam");
+
+
+//var childOfEthernet0 = rootConfig.children[2].getChildren();
 // InsertNodeUnderItsParent('version', rootConfig);
 // var version = rootConfig.children[0];
 // InsertNodeUnderItsParent('15.5', version);
@@ -238,6 +277,5 @@ function InsertNodeUnderItsParent(node, parent){
 // InsertNodeUnderItsParent('debug', serviceTimestamps);
 // InsertNodeUnderItsParent('log', serviceTimestamps);
 //FindParentOfANode(["service", "timestamp"], 'log');
-console.log("salam");
 
 
